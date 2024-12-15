d3d90eadc2c4ef49be11425ef7d64509cfb4f4ed
Bryan <bhad778@gmail.com>
2024-12-15 15:48:42 -0500

    fixes

---
+++ packages/keeperServices/ThirdPartyService.ts
@@ -0,0 +1,33 @@
+import { affindaKey, affindaWorkspaceId } from 'keeperEnvironment';
+
+const useThirdPartyService = () => {
+  const uploadResumeToParser = (base64: string | Blob) => {
+    const form = new FormData();
+    form.append('wait', 'true');
+    form.append('rejectDuplicates', 'false');
+    form.append('file', base64);
+    form.append('workspace', affindaWorkspaceId);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        authorization: `Bearer ${affindaKey}`,
+      },
+    };
+    options.body = form;
+    return fetch('https://api.affinda.com/v3/documents', options)
+      .then(response => response.json())
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+  return {
+    uploadResumeToParser,
+  };
+};
+
+export default useThirdPartyService;
---
+++ packages/keeperServices/index.ts
@@ -0,0 +1,4 @@
+export * as JobsService from './JobsService';
+export * as MiscService from './JobsService';
+export * as UsersService from './JobsService';
+export * as ThirdPartService from './ThirdPartyService';
